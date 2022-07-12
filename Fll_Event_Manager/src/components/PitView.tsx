3740b132a2a77d11f291d281b00b2f45a52c40a5
clayton.midgley@gmail.com <clayton.midgley@gmail.com>
2022-07-12 13:01:59 -0400

    Started PCB design and made small code changes

--- Fll_Event_Manager/src/components/PitView.tsx
+++ Fll_Event_Manager/src/components/PitView.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import ClientClock from "./ClientClock";
 import MatchCounter from "./MatchCounter";
-//import "./ControlButtons.css";
 
 const PitView = () => {
-    return(
-        <div>
-            <h1>Pit view</h1>
-            <ClientClock></ClientClock>
-            <MatchCounter></MatchCounter>
+    return (
+        <div className="bg-light h-100 mx-auto fixed-top SetWidth">
+            <div className="text-center">
+                <ClientClock></ClientClock>
+            </div>
+
+            <div className="MatchCounter_Pit">
+                <MatchCounter></MatchCounter>
+            </div>
         </div>
+
     )
 }
 
